28402fb26a562df28fc798cf5a90f298c76abd7a
Shayan Hassan <gamingfanatics2115@gmail.com>
2025-09-04 14:58:28 +0500

    UserInput & Then Print

---
+++ 1_Problem.py
@@ -0,0 +1,14 @@
+# Taking input from user and then print the dic
+
+dic={}
+item=int(input("How many Items Do You Want In Dic : "))
+
+for i in range(item):
+    key=(input("Enter Your Name : "))
+    value=int(input("Enter Marks : "))
+
+    dic[key]=value
+
+print("Your Names and Marks ")
+for  i ,k in dic.items():
+    print(i," ",k)
